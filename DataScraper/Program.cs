437a359e49b358753f85a8b0554b1dab98ba6e22
Gilles Leblanc <gilles.leblanc@gmail.com>
2018-01-01 12:20:05 -0500

    Start of work on DataScraper
    Commented out section seems to work but I have commented it out so I can focus on a single page for tests.

--- DataScraper/Program.cs
+++ DataScraper/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using DataScraper.Models;
+using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 
 namespace DataScraper
 {
-    class Program
+    public class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+        private static HttpClient httpClient;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            httpClient = new HttpClient();
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                    .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            IConfigurationSection monsterUrlsSection = Configuration.GetSection("monsterUrls");
+            var monsterUrls = monsterUrlsSection.AsEnumerable();
+
+            monsterUrls.Where(x => !string.IsNullOrWhiteSpace(x.Value))
+                       //.Select(hub => DownloadPage(hub.Value))
+                       //.SelectMany(html => GetAllLinks(html))
+                       //.Where(link => IsMonsterPageLink(link))
+                       //.Select(mob => DownloadPage(mob))
+                       .Select(mob => DownloadPage("http://www.d20pfsrd.com/bestiary/monster-listings/constructs/clockwork/clockwork-reliquary/"))
+                       .Where(html => IsValidMonster(html))
+                       .Select(html => new Monster(html))
+                       .ToList()
+                       .ForEach(x => WriteToFile(x));
         }
+
+        private static string DownloadPage(string url)
+        {
+            try
+            {
+                return httpClient.GetStringAsync(url).Result;
+            }
+            catch (Exception)
+            {
+                // if we can't download the page, ie: broken link, continue on with the other pages
+                return string.Empty;
+            }
+        }
+
+        private static IEnumerable<string> GetAllLinks(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            return document.DocumentNode.SelectNodes("//a[@href]").Select(a => a.GetAttributeValue("href", null))
+                                                                  .Where(lnk => !string.IsNullOrWhiteSpace(lnk)); 
+        }
+
+        private static bool IsMonsterPageLink(string link) => link.Contains("/bestiary/monster-listings/");
+
+        private static bool IsValidMonster(string html) => html.Contains("XP ") && html.Contains("Init ");
+
+        private static void WriteToFile(Monster monster) => File.WriteAllText($"./output/{monster.Name}.json", monster.ToJson());
     }
 }
---
+++ DomainModels/Monster.cs
@@ -0,0 +1,74 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace DataScraper.Models
+{
+    public class Monster
+    {
+        private const string str = "Str";
+        private const string dex = "Dex";
+        private const string con = "Con";
+        private const string @int = "Int";
+        private const string wis = "Wis";
+        private const string cha = "Cha";
+
+        public string Name { get; set; }
+
+        public int Ac { get; set; }
+
+        public int Hp { get; set; }
+
+        public int Str { get; set; }
+
+        public int Dex { get; set; }
+
+        public int Con { get; set; }
+
+        public int Int { get; set; }
+
+        public int Wis { get; set; }
+
+        public int Cha { get; set; }
+
+        public Monster(string html)
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var statsBlock = document.DocumentNode
+                                     .Descendants("p")
+                                     .FirstOrDefault(p => p.InnerHtml.Contains("<b>Str </b>"));
+
+            if (statsBlock == null)
+                throw new InvalidOperationException("Could not find stats block.");
+
+            Str = GetStat(statsBlock, str);
+            Dex = GetStat(statsBlock, dex);
+            Con = GetStat(statsBlock, con);
+            Int = GetStat(statsBlock, @int);
+            Wis = GetStat(statsBlock, wis);
+            Cha = GetStat(statsBlock, cha);
+        }
+
+        private static int GetStat(HtmlNode statsBlock, string stat)
+        {
+            var possibleValue = statsBlock.Descendants()
+                                          .SingleOrDefault(d => d.Name == "b" && d.InnerHtml == stat + " ")
+                                          ?.NextSibling
+                                          ?.InnerText
+                                          .Replace('—', '0')
+                                          .Trim()
+                                          .TrimEnd(',');
+
+            int parsedValue = 0;
+            if (!int.TryParse(possibleValue, out parsedValue))
+                throw new InvalidOperationException($"Could not parse {possibleValue} for stat {stat} in statsblock {statsBlock}.");
+
+            return parsedValue;
+        }
+
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+}
